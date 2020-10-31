9eb2952cc1af1ccf9266a188d3368dd1bfca30ae
Administrator <yangfanqikang@163.com>
2020-10-31 23:38:46 +0800

    构建基本要素

---
+++ src/components/draft/business/countersign.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="countersign">
+    countersign
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "countersign"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
